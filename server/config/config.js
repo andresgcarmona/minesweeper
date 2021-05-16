eb9ab3ecec4de4adade58d251b027c01de9073f8
Andres G. Carmona <ing.andres.carmona@gmail.com>
2021-05-16 13:57:08 -0500

    Added config file to store global game and app configurations.

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+const gameConfig = {
+  beginner: {
+    boardSize: [10, 10],
+    numMines: 10,
+  },
+  intermediate: {
+    boardSize: [20, 20],
+    numMines: 40,
+  },
+  advanced: {
+    boardSize: [30, 20],
+    numMines: 60,
+  },
+}
+module.exports = {
+  gameConfig,
+}
