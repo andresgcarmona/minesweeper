f7f5d2853cf4938d2ab3e0bed2156b4f93adc84f
Andres G. Carmona <ing.andres.carmona@gmail.com>
2021-05-17 17:18:37 -0500

    Using getGameById middleware in check route.

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const router  = new express.Router()
-
-const gameController = require('../controllers/game')
+const express         = require('express')
+const router          = new express.Router()
+const { getGameById } = require('../utils/game')
+const gameController  = require('../controllers/game')
 
 // Index of game section.
 router.get('/', gameController.index)
 
 // Create a new game
 router.post('/', gameController.create)
+
+// Check a cell in the board.
+router.post('/check', getGameById, gameController.checkCell)
 
 module.exports = router
