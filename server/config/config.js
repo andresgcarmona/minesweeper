c125bc485d89161f55b41ee83193267a182b5126
Andres G. Carmona <ing.andres.carmona@gmail.com>
2021-05-17 13:56:07 -0500

    Changed beginner's numMines to 9 and advanced board size.

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
 const gameConfig = {
   beginner: {
     boardSize: [10, 10],
-    numMines: 10,
+    numMines: 9,
   },
   intermediate: {
     boardSize: [20, 20],
     numMines: 40,
   },
   advanced: {
-    boardSize: [30, 20],
+    boardSize: [30, 30],
     numMines: 60,
   },
 }
